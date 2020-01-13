b92464e4f9d071986a36ed536276755052fc2565
Neta Li Lahat <nllahat@gmail.com>
2020-01-14 00:21:41 +0200

    views

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,8 @@ const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
